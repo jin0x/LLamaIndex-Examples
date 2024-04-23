222df39c7f0daea40a02f770ad3629bc6c509778
John Leskas <john.leskas@gmail.com>
2024-04-23 21:22:55 +0300

    chore: llm settings updates

--- OpenAI-Starter/main.py
+++ OpenAI-Starter/main.py
@@ -7,17 +7,21 @@ from llama_index.core import (
     load_index_from_storage
 )
 from llama_index.llms.ollama import Ollama
+from llama_index.core.node_parser import SentenceSplitter
 from dotenv import load_dotenv
 
 load_dotenv()
 
+text_splitter = SentenceSplitter(chunk_size=200, chunk_overlap=10)
+
+Settings.text_splitter = text_splitter
 Settings.llm = Ollama(model="mistral")
 
 PERSIST_DIR = "./storage"
 if not os.path.exists(PERSIST_DIR):
     # load the documents and create the index
     documents = SimpleDirectoryReader('data').load_data()
-    index = VectorStoreIndex.from_documents(documents, show_progress=True)
+    index = VectorStoreIndex.from_documents(documents, show_progress=True, transformations=[text_splitter])
     index.storage_context.persist()
 else:
     # load the existing index
