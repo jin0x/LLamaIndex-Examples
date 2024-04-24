5cb18fd794d1822707940b15839bc74bcc308eba
John Leskas <john.leskas@gmail.com>
2024-04-24 22:22:28 +0300

    chore: change model

--- OpenAI-Starter/starter.py
+++ OpenAI-Starter/starter.py
@@ -4,19 +4,21 @@ from llama_index.core import (
     Settings,
     SimpleDirectoryReader,
     StorageContext,
-    load_index_from_storage
+    load_index_from_storage,
 )
 from llama_index.llms.ollama import Ollama
+from llama_index.llms.openai import OpenAI
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.evaluation import FaithfulnessEvaluator
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 load_dotenv()
 
 text_splitter = SentenceSplitter(chunk_size=200, chunk_overlap=10)
 
 Settings.text_splitter = text_splitter
-Settings.llm = Ollama(model="mistral")
+# Settings.llm = Ollama(model="mistral")
+Settings.llm = OpenAI()
 
 PERSIST_DIR = "./storage"
 if not os.path.exists(PERSIST_DIR):
@@ -35,7 +37,9 @@ evaluator = FaithfulnessEvaluator(llm=Settings.llm)
 # Either way we can now query the index
 query_engine = index.as_query_engine()
 response = query_engine.query("What did the author do growing up?")
+
 print(response)
+print('\n\n\n')
 
 eval_result = evaluator.evaluate_response(response=response)
 print(eval_result)
